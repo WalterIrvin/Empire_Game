1d64205e32a75384a5fa822ca741c96bf594a10a
WalterIrvin <walterdirvin@gmail.com>
2020-06-25 14:00:36 -0400

    added health/hunger/thirst

--- Assets/Scripts/Entities/Player.gd
+++ Assets/Scripts/Entities/Player.gd
@@ -2,16 +2,67 @@ extends KinematicBody2D
 var inventory = {"flint": 0, "wood": 0, "stick": 0}
 var velocity = Vector2(0, 0)
 var speed = 100
+var health = 100
+var max_health = 100
+var hunger = 100
+var max_hunger = 100
+var thirst = 100
+var max_thirst = 100 
+var hunger_rate = 10.05 # how much hunger per second to remove
+var thirst_rate = 0.09  # how much thirst per second to remove
+var starve_damage = 100.1  # how much damage per second from starving
+var dehydrate_damage = 0.5  # how much damage per second from dehydration
 var inventory_ref
 var hotbar_ref
+var health_ref
+var hunger_ref
+var thirst_ref
 
 func _ready():
 	inventory_ref = find_node("UI")
 	hotbar_ref = find_node("Hotbar")
+	health_ref = find_node("HealthBar")
+	hunger_ref = find_node("HungerBar")
+	thirst_ref = find_node("ThirstBar")
+	update_bars()
 
-func _process(_delta):
+func tick_needs(delta):
+	# drains hunger / thirst by decay amt * delta
+	if hunger > 0:
+		hunger -= delta * hunger_rate
+	if thirst > 0:
+		thirst -= delta * thirst_rate
+	if hunger <= 0:
+		hunger = 0
+		health -= delta * starve_damage 
+	if thirst <= 0:
+		thirst = 0
+		health -= delta * dehydrate_damage
+	if health <= 0:
+		health = 0
+
+func update_bars():
+	update_health()
+	update_hunger()
+	update_thirst()
+
+func update_health():
+	var pct = health / max_health
+	health_ref.value = pct * 100
+	
+func update_hunger():
+	var pct = hunger / max_hunger
+	hunger_ref.value = pct * 100
+
+func update_thirst():
+	var pct = thirst / max_thirst
+	thirst_ref.value = pct * 100
+
+func _process(delta):
 	process_input()
 	velocity = move_and_slide(velocity)
+	tick_needs(delta)
+	update_bars()
 	
 func get_hotbar():
 	return hotbar_ref
@@ -20,15 +71,18 @@ func process_input():
 	velocity = Vector2(0, 0)
 	if Input.is_action_pressed("up"):
 		velocity.y = -speed
-		
 	if Input.is_action_pressed("down"):
 		velocity.y = speed
-		
 	if Input.is_action_pressed("left"):
 		velocity.x = -speed
-		
 	if Input.is_action_pressed("right"):
 		velocity.x = speed
+	if hunger <= 0:
+		velocity *= 0.5
+	if thirst <= 0:
+		velocity *= 0.5
+	if health <= 0:
+		velocity *= 0
 
 func add_item(res_type, amt):
 	inventory_ref.add_item(res_type, amt)
--- Assets/Scripts/Utility/InventorySlot.gd
+++ Assets/Scripts/Utility/InventorySlot.gd
@@ -14,14 +14,25 @@ var activated = false  # used for hotbar slots
 
 
 func _ready():
-	var player = get_tree().get_root().get_node("LevelRoot").find_node("Player")
-	floating_ref = player.find_node("FloatCan").find_node("FloatingSelection")
+	find_player()
 	img_ref = get_child(1)
 	amt_ref = get_child(2)
 	if res_amt == 0 or res_amt == 1:
 		amt_ref.text = ""
-
+		
+func find_player():
+	var root_node = get_tree().get_root().get_node("LevelRoot")
+	if floating_ref == null:
+		for c in range(root_node.get_child_count()):
+			print(root_node.get_child(c).name)
+	var player = root_node.find_node("Player")
+	print(player)
+	if player != null:
+		floating_ref = player.find_node("FloatCan").find_node("FloatingSelection")
+		
 func _process(_delta):
+	if floating_ref == null:
+		find_player()
 	img_ref.texture = tile_texture
 	if res_amt >= 2:
 		amt_ref.text = "x" + str(res_amt)
@@ -44,72 +55,76 @@ func swap_selection(click_type):
 	# If selection same type as inventory slot, take from selection and put into
 	# the inventory slot, up until the inventory slot max amt is reached
 	###
-
-	if click_type == 1:
-		# RIGHT CLICK
-		if floating_ref.res_type == "none":
-			# Case 1 - empty selection take half of slot (ceiling round)
-			var raw = res_amt / 2.0
-			if raw >= 1.0:
-				# can be split
-				var half = ceil(raw)
-				res_amt -= half
-				set_selection()
-				floating_ref.res_amt = half
-			else:
-				# can't be split, just one item
+	if not is_instance_valid(floating_ref):
+		floating_ref = null
+		find_player()
+		return
+	else:
+		if click_type == 1:
+			# RIGHT CLICK
+			if floating_ref.res_type == "none":
+				# Case 1 - empty selection take half of slot (ceiling round)
+				var raw = res_amt / 2.0
+				if raw >= 1.0:
+					# can be split
+					var half = ceil(raw)
+					res_amt -= half
+					set_selection()
+					floating_ref.res_amt = half
+				else:
+					# can't be split, just one item
+					set_selection()
+					clear_self()
+				
+			elif floating_ref.res_type == res_type or res_type == "none":
+				# Case 2 - put one item into the slot, if not past max cap
+				if res_amt + 1 <= stack_max:
+					floating_ref.res_amt -= 1
+					res_type = floating_ref.res_type
+					res_amt += 1
+					tile_texture = floating_ref.tile_texture
+					floating_ref.tool_type = tool_type
+					floating_ref.tool_quality = tool_quality
+					if floating_ref.res_amt <= 0:
+						clear_selection()
+				else:
+					swap_elements()
+	
+			elif floating_ref.res_type != res_type:
+				# Case 3 - swap elements
+				swap_elements()
+				
+		elif click_type == 0:
+			# LEFT CLICK
+			if floating_ref.res_type == "none":
+				# Case 1 - empty selection take whole stack
 				set_selection()
 				clear_self()
-			
-		elif floating_ref.res_type == res_type or res_type == "none":
-			# Case 2 - put one item into the slot, if not past max cap
-			if res_amt + 1 <= stack_max:
-				floating_ref.res_amt -= 1
-				res_type = floating_ref.res_type
-				res_amt += 1
-				tile_texture = floating_ref.tile_texture
-				floating_ref.tool_type = tool_type
-				floating_ref.tool_quality = tool_quality
-				if floating_ref.res_amt <= 0:
-					clear_selection()
-			else:
-				swap_elements()
-
-		elif floating_ref.res_type != res_type:
-			# Case 3 - swap elements
-			swap_elements()
-			
-	elif click_type == 0:
-		# LEFT CLICK
-		if floating_ref.res_type == "none":
-			# Case 1 - empty selection take whole stack
-			set_selection()
-			clear_self()
-			
-		elif floating_ref.res_type == res_type:
-			# Case 2 - selection same as slot, fill slot up to stack max
-			var dif = stack_max - res_amt
-			if dif == 0:
-				# swap selection and slot
-				swap_elements()
-			if dif >= floating_ref.res_amt:
-				# takes all of floating selection to fill
-				res_amt += floating_ref.res_amt
-				clear_selection()
-			else:
-				# takes some of floating selection to fill
-				var remainder = floating_ref.res_amt - dif
-				res_amt += dif
-				floating_ref.res_amt = remainder
 				
-		elif res_type == "none" and floating_ref.res_type != "none":
-			# Case 3 - nothing in slot, put the selection in it
-			set_slot()
-			clear_selection()
-			
-		elif res_type != floating_ref.res_type:
-			# Case 4 - different things in selection and slot, swap them
-			swap_elements()
+			elif floating_ref.res_type == res_type:
+				# Case 2 - selection same as slot, fill slot up to stack max
+				var dif = stack_max - res_amt
+				if dif == 0:
+					# swap selection and slot
+					swap_elements()
+				if dif >= floating_ref.res_amt:
+					# takes all of floating selection to fill
+					res_amt += floating_ref.res_amt
+					clear_selection()
+				else:
+					# takes some of floating selection to fill
+					var remainder = floating_ref.res_amt - dif
+					res_amt += dif
+					floating_ref.res_amt = remainder
+					
+			elif res_type == "none" and floating_ref.res_type != "none":
+				# Case 3 - nothing in slot, put the selection in it
+				set_slot()
+				clear_selection()
+				
+			elif res_type != floating_ref.res_type:
+				# Case 4 - different things in selection and slot, swap them
+				swap_elements()
 
 func swap_elements():
 	var tmp_res_type = res_type
