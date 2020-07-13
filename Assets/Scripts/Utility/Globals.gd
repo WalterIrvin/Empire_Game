3e3cec5445215b8131b38e4c73aa25693ed16ef0
WalterIrvin <walterdirvin@gmail.com>
2020-07-12 22:53:23 -0400

    First pass on the new pattern/recipe system, following similar structure to the item dictionary, a recipe dictionary keeps tracks of all the possible recipes. Further development may go to the recipe syntax, to potentially include time to craft.

--- Assets/Scripts/Utility/Globals.gd
+++ Assets/Scripts/Utility/Globals.gd
@@ -52,6 +52,61 @@ class ItemDict:
 		# wrapper for dictionary.has
 		return self.item_dictionary.has(id)
 
+class Recipe:
+	var recipe_id = 0  #id in the recipe dictionary
+	var id_amt_pair = []  #2d array [[id:amt], ...]
+	var output_id = 0  #the id of the final crafted item
+	func _init(input_id, pairs, crafted_id):
+		recipe_id = input_id
+		id_amt_pair = pairs
+		output_id = crafted_id
+	
+	func craft(amt):
+		# checks the inventory of player to see if they have the needed
+		# ingredients to make create the item. Crafts item up to amt if possible
+		if Globals.Global_Inventory != null:
+			pass
+
+class RecipeDict:
+	var recipe_dict = {}
+	func _init(path):
+		var file = File.new()
+		if file.open(path, File.READ) != 0:
+			print("error opening file")
+			return
+		var begin_read = false
+		while not file.eof_reached():
+			var line = file.get_line()
+			if begin_read:
+				var side = line.split(";")
+				# right hand side
+				var output_id = int(side[1])
+				# left hand side
+				var input = side[0]
+				var pattern = input.split(",")
+				# final vars + splitting id:amt lists
+				var recipe_id = int(pattern[0])
+				var id_amt_pairs = []
+				for i in range(1, len(pattern)):
+					var cur_pair = pattern[i].split(":")
+					id_amt_pairs.append([int(cur_pair[0]), int(cur_pair[1])])
+				# recipe building section
+				var new_recipe = Recipe.new(recipe_id, id_amt_pairs, output_id)
+				recipe_dict[recipe_id] = new_recipe
+			if line == "[RECIPE_SECTION]":
+				begin_read = true
+		file.close()
+	
+	func get_recipe(id):
+		if recipe_dict.has(int(id)):
+			return recipe_dict[int(id)]
+		else:
+			return null
+	
+	func has(id):
+		return recipe_dict.has(id)
+
+
 class PlayerData:
 	var speed = 100
 	var health = 100
@@ -66,7 +121,9 @@ class PlayerData:
 	var dehydrate_damage = 0.5
 
 var path = "res://Assets/Data/item_info.txt"
+var recipe_path = "res://Assets/Data/recipe_info.txt"
 var Global_Item_Dictionary = ItemDict.new(path)
+var Global_Recipe_Dictionary = RecipeDict.new(recipe_path)
 var Global_Player_Data = PlayerData.new()
 var Global_Player_Ref
 var Global_Hotbar
@@ -75,3 +132,4 @@ var Global_Hungerbar
 var Global_Thirstbar
 var Global_Inventory
 var Global_Floating_Ref
+
